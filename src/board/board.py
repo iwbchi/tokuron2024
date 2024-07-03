b5802d14ead72c27eb0f04e3cf044a30a96aef6c
iwbchi <hfffdde2004@gmail.com>
2024-07-03 14:21:38 +0900

    fix use Agents when init Board

--- src/board/board.py
+++ src/board/board.py
@@ -1,5 +1,7 @@
 # 盤面情報
 import numpy as np
+from Agent import Agent, AgentType, Agents
+from Utils import Point
 
 
 class Board:
@@ -24,7 +26,7 @@ class Board:
         )
         # 0 : 中立, 1 : 陣地,  2 : 城壁
         self.board_territory_ally = np.zeros((13, 13))
-        self.board_territory_enemy = np.zeros(13, 13)
+        self.board_territory_enemy = np.zeros((13, 13))
 
         # 0:空白, 1:味方エージェント, 2: 敵エージェント
         self.board_agent = np.array(
@@ -44,3 +46,25 @@ class Board:
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             ]
         )  # 0 : いない, 1 : 味方, 2 : 相手
+
+        self._init_agents()
+
+    def _init_agents(self):
+        self.agents_ally = Agents(
+            AgentType.ally,
+            [
+                Agent(1, Point(6, 1)),
+                Agent(2, Point(4, 6)),
+                Agent(3, Point(8, 6)),
+                Agent(4, Point(6, 11)),
+            ],
+        )
+        self.agents_enemy = Agents(
+            AgentType.enemy,
+            [
+                Agent(1, Point(6, 4)),
+                Agent(2, Point(1, 6)),
+                Agent(3, Point(11, 6)),
+                Agent(4, Point(6, 8)),
+            ],
+        )
