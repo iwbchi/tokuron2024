59612b314b7fdbedebb8bf034a5ede7e7d518fb3
iwbchi <hfffdde2004@gmail.com>
2024-09-23 20:16:09 +0900

    fix montecarlo

--- src/board/montecarlo.py
+++ src/board/montecarlo.py
@@ -1,29 +1,39 @@
-from board import Board
-import score
-import numpy as np
+import copy
+import random
 
-def montecarlo_action(board):
-    max_score = 0
+from Board import Board
+from Agent import AgentType
+from Action import Actions
+import score
+
+
+def montecarlo_action(board: Board, turn: int) -> Actions:
+    max_score: float = 0
     max_actions = None
-    nxt_actions = [] # TODO: 列挙したやつ入れる
-    for actions in nxt_actions:
+    for actions in board.get_actions():
         # TODO: actionsを適用する
-        nxt_board = ...
-        score = playout(nxt_board, 100)
+        nxt_board = copy.deepcopy(board)
+        nxt_board.op_actions(actions)
+        score = playout(nxt_board, 100, turn)
         if score > max_score:
             max_score = score
             max_actions = actions
-    
+
     return max_actions
 
-def playout(board, n: int):
+
+def playout(board: Board, n: int, turn: int) -> float:
     sum_score = 0
+    actions_list = board.get_actions()
     for _ in range(n):
-        while not board.isDone():
+        for i in range(turn, 30):
+            action_id = random.randint(0, len(actions_list) - 1)
+            actions = actions_list[action_id]
             # アクションをランダムに適用する。
-            pass
+            agent_type = AgentType.ally if i % 2 == 0 else AgentType.enemy
+            board.op_actions(actions, agent_type)
         ally_score = score.get_score(board, True)
         enemy_score = score.get_score(board, False)
-        sum_score += abs(ally_score - enemy_score)
-        
+        sum_score += ally_score - enemy_score
+
     return sum_score / n
